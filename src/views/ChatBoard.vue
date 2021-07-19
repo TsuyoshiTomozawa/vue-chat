5f84b87fe5508d678386d777e06a16ca5aa558e7
Tsuyoshi Tomozawa <sphdm244@gmail.com>
2021-07-20 08:48:30 +0900

    feat: ユーザー一覧など実装

---
+++ src/views/ChatBoard.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-app id="inspire">
+    <v-main>
+      <v-container class="py-8 px-6" fluid>
+        <v-row>
+          <v-col cols="12">
+            <v-card>
+              <!-- <v-subheader>{{ card }}</v-subheader> -->
+
+              <v-list two-line>
+                <template v-for="(data, index) in messages">
+                  <v-list-item :key="index">
+                    <v-list-item-avatar color="grey darken-1">
+                    </v-list-item-avatar>
+
+                    <v-list-item-content>
+                      <!-- <v-list-item-title>Message! {{ n }}</v-list-item-title> -->
+
+                      <v-list-item-subtitle class="message">
+                        {{ data.message }}
+                      </v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-divider
+                    v-if="n !== 6"
+                    :key="`divider-${index}`"
+                    inset
+                  ></v-divider>
+                </template>
+              </v-list>
+            </v-card>
+          </v-col>
+          <v-textarea
+            append-icon="mdi-comment"
+            class="mx-2"
+            auto-grow
+            label="メッセージを送信する"
+            rows="1"
+          ></v-textarea>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+// import Chat from "@/components/Chat.vue";
+
+export default {
+  data: () => ({
+    drawer: null,
+    links: [
+      ["mdi-inbox-arrow-down", "Inbox"],
+      ["mdi-send", "Send"],
+      ["mdi-delete", "Trash"],
+      ["mdi-alert-octagon", "Spam"],
+    ],
+    messages: [
+      { message: "message1" },
+      { message: "message2" },
+      { message: "message3" },
+      { message: "message4" },
+    ],
+  }),
+};
+</script>
+
+<style scoped>
+.message {
+  text-align: left;
+}
+</style>
